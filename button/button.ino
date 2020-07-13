f7c506d7038250f5f1437bce1e71bc194853d814
chuong <trandinhchuonght96@gmail.com>
2020-07-13 20:12:15 +0700

    .

--- button/button.ino
+++ button/button.ino
@@ -1,5 +1,5 @@
-int button = 5;
 
+int button = 4; //D2(gpio4)
 void setup() {
   Serial.begin(9600);   //Mở cổng Serial ở baudrate 9600 để giao tiếp với máy tính
   pinMode(button, INPUT);  //Cài đặt chân button ở trạng thái đọc dữ liệu

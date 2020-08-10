6df4336df66fe9c97a1a68f390927d039be2ea7d
chuong <trandinhchuonght96@gmail.com>
2020-08-10 21:01:28 +0700

    ssd1306

---
+++ ssd1306/ssd1306.ino
@@ -0,0 +1,60 @@
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 //  Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+static const unsigned char PROGMEM logo_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+
+void setup() {
+  Serial.begin(9600);
+   Wire.begin(D1,D2); // (SDA - D1 / SCL -D2)
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+display.clearDisplay();
+
+display.setTextSize(1); //Set our text size, size 1 correlates to 8pt font
+display.setTextColor(WHITE); //We're using a Monochrome OLED so color is irrelevant, pixels are binary.
+display.setCursor(0,0); //Start position for the font to appear
+display.println("HELLO WORD"); //Will display text on new line
+
+display.drawBitmap(10, 10,  logo_bmp, 16, 16, 1);
+
+display.display();
+
+}
+
+void loop() {
+}
